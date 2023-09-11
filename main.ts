96d7cf89513fb92ceac674b2452020a47c1ca40f
B33Boy <abhip202@gmail.com>
2023-09-10 23:24:10 -0400

    Handle paste event

--- main.ts
+++ main.ts
@@ -1,81 +1,47 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { 
+	App,  
+	Editor, 
+	MarkdownView, 
+	Modal, 
+	Plugin, 
+	Notice,
+	PluginSettingTab, 
+	Setting} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface HandlePaste {
+	(this: HTMLElement, ev: ClipboardEvent, editor: Editor): void;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface CompressSettings {
+	compressRatio: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: CompressSettings = {
+	compressRatio: "0.5"
+}
+
+export default class Compress extends Plugin {
+	settings: CompressSettings;
+
+	handlePaste: HandlePaste;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("image-minus", "Compress",() => {
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.handlePaste = this.updatePastedImage.bind(this);
+		
+		this.registerEvent(
+			this.app.workspace.on("editor-paste", this.handlePaste)
+		);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new CompressSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -89,28 +55,32 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	// Utility functions go below ======================================================================
+	async updatePastedImage(ev: ClipboardEvent, editor: Editor): Promise<void>
+	{
+		if (ev.defaultPrevented) {
+			return;
+		}
+		ev.preventDefault();
+
+		  // Get text from editor to get source url of image
+		  
+		  // Represent image using TFile 
+
+		  // Use some API to compress the image
+
+		  // Paste the image
+
+		  new Notice("Pasted an Image"); // Check if the pasted 
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CompressSettingsTab extends PluginSettingTab {
+	plugin: Compress;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Compress) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +91,15 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Compression Ratio')
+			.setDesc('Compression Ratio')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+				.setPlaceholder('Compression Ratio')
+				.setValue(this.plugin.settings.compressRatio)
+				.onChange(async (value: string) => {
+					this.plugin.settings.compressRatio = value;
 					await this.plugin.saveSettings();
-				}));
+				})); 
 	}
 }
+ 
