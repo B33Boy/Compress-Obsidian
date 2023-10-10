2d108d7d72a887d556db63c5fd129f988fba7fe5
B33Boy <abhip202@gmail.com>
2023-10-09 20:42:34 -0400

    handle paste event semi-successfully

---
+++ imgprocess.ts
@@ -0,0 +1,13 @@
+// const sharp = require('sharp');
+
+// export default function compressImage(filePath: string) {
+//     return filePath;
+
+//     // sharp(filePath)
+//     //     .jpeg({ progressive: true, force: false })
+//     //     .png({ progressive: true, force: false });
+// }
+
+export default function compressImage(buffer: ArrayBuffer) {
+    
+}
--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 import { 
 	App,  
 	Editor, 
-	MarkdownView, 
-	Modal, 
-	Plugin, 
+	MarkdownView,
+	MarkdownFileInfo, 
 	Notice,
+	Plugin, 
 	PluginSettingTab, 
-	Setting} from 'obsidian';
+	Setting,
+	TFile,
+	TFolder} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import compressImage from './imgprocess';
 
-interface HandlePaste {
-	(this: HTMLElement, ev: ClipboardEvent, editor: Editor): void;
-}
 
 interface CompressSettings {
 	compressRatio: string;
@@ -25,18 +24,19 @@ const DEFAULT_SETTINGS: CompressSettings = {
 export default class Compress extends Plugin {
 	settings: CompressSettings;
 
-	handlePaste: HandlePaste;
+	// handlePaste: HandlePaste;
 
 	async onload() {
 		await this.loadSettings();
 
 		this.addRibbonIcon("image-minus", "Compress",() => {
+			console.log("Does nothing lol");
 		});
 
-		this.handlePaste = this.updatePastedImage.bind(this);
+		// this.handlePaste = this.updatePastedImage.bind(this);
 		
 		this.registerEvent(
-			this.app.workspace.on("editor-paste", this.handlePaste)
+			this.app.workspace.on("editor-paste", this.updatePastedImage.bind(this))
 		);
 
 
@@ -57,22 +57,68 @@ export default class Compress extends Plugin {
 	}
 
 	// Utility functions go below ======================================================================
-	async updatePastedImage(ev: ClipboardEvent, editor: Editor): Promise<void>
+	async updatePastedImage(ev: ClipboardEvent, editor: Editor, view: MarkdownView): Promise<void>
 	{
-		if (ev.defaultPrevented) {
+		if (ev.defaultPrevented || !ev.clipboardData) return;
+
+		let types = ev.clipboardData.types;
+		
+		if (types && types.includes("Files")){
+
+			const imgFiles = Array.from(ev.clipboardData.files);
+			const img = imgFiles.find(file => file.type.startsWith("image/"));
+			
+			if (img){
+				ev.preventDefault();
+				
+				let fileData = "";
+				console.log("Image Found! Type: " + img.type);
+				
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					let fileData = reader.result as string;
+
+					console.log("READ DATA: ", fileData)
+
+					const randomVal = Math.floor(Math.random() * 1000);
+					let imgFileName: string = `temp-image-name-${randomVal}.png`;
+
+					this.saveImage(imgFileName, fileData);
+
+					// call compressImage
+					const imgLink = `![[${imgFileName}]]`;
+					editor.replaceSelection(imgLink);
+				};
+				
+				reader.readAsText(img);
+				
+				// const randomVal = Math.floor(Math.random() * 100);
+				// let imgFileName: string = `temp-image-name-${randomVal}.png`;
+
+				// console.log(`${imgFileName} and ${fileData}`);
+				// this.saveImage(imgFileName, fileData);
+
+				// // call compressImage
+				// const imgLink = `![[${imgFileName}]]`;
+				// editor.replaceSelection(imgLink);
+			}			
+		}
+		console.log("Fn finished");
+	}
+
+	async saveImage(fileName: string, imageData: string): Promise<void> {
+		//get current folder path
+		const activeFile: TFile | null = this.app.workspace.getActiveFile();
+
+		if (!activeFile || !activeFile.parent) {
+			// throw error
+			new Notice("No active file");
 			return;
 		}
-		ev.preventDefault();
 
-		  // Get text from editor to get source url of image
-		  
-		  // Represent image using TFile 
-
-		  // Use some API to compress the image
-
-		  // Paste the image
-
-		  new Notice("Pasted an Image"); // Check if the pasted 
+		const currentFolderPath: string | null = activeFile.parent.path;
+		const arrayBuffer = new TextEncoder().encode(imageData).buffer;
+		await this.app.vault.createBinary(`${currentFolderPath}/${fileName}`, arrayBuffer);
 	}
 
 }
